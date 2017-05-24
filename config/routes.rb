befd6de75b9bb981ea37731e1cb51b83156b1ce6
Lukas Szymanski <l.szymanski@neusta.de>
2017-05-24 14:01:10 +0200

    Finish static pages Ch3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'foo/bar'  
+  get 'foo/baz'  
+  get 'static_pages/home'  
+  get 'static_pages/help'  
+  get 'static_pages/about'
+  get 'static_pages/contact'
 
-  get 'static_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+	root 'static_pages#home'
 
 end
