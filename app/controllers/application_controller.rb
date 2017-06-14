aeb53f6fb02a6fec52da8687e314cb3201ccafb4
Lukas Szymanski <l.szymanski@neusta.de>
2017-06-14 15:33:01 +0200

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
-	include SessionsHelper
+  private
 
-	def hello
-		render html: "hello, world!"
-	end
-	
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-
+# PictureUploader ...
+require 'carrierwave/orm/activerecord'
 # Initialize the Rails application.
 Rails.application.initialize!
